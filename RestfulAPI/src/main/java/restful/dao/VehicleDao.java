d8dc96472056d4f70431adf4cdb5d0426f02c531
Yesi <yex4@pitt.edu>
2021-01-28 09:02:12 -0500

    remove code

--- RestfulAPI/src/main/java/restful/dao/VehicleDao.java
+++
@@ -1,7 +0,0 @@
-package restful.dao;
-
-import restful.model.Vehicle;
-
-public interface VehicleDao {
-	boolean submitVehicle(Vehicle vehicle);
-}
--- RestfulAPI/src/main/java/restful/service/VehicleService.java
+++
@@ -1,7 +0,0 @@
-package restful.service;
-
-import restful.model.Vehicle;
-
-public interface VehicleService {
-	boolean submitVehicle(Vehicle vehicle);
-}
