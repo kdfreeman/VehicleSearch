34698a95be83393b064ed346f957d64b3ff1fb76
Kapil Dahal <kapild@mtu.edu>
2021-03-02 14:25:36 -0500

    Implemented Feature Search In The Vehicle Repository

---
+++ src/main/java/com/kapilfreeman/vehiclesearch/service/randomserialize.java
@@ -0,0 +1,28 @@
+//package com.kapilfreeman.vehiclesearch.service;
+//
+//import com.fasterxml.jackson.core.JsonGenerator;
+//import com.fasterxml.jackson.core.JsonParser;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.core.ObjectCodec;
+//import com.fasterxml.jackson.databind.*;
+//import com.kapilfreeman.vehiclesearch.model.VehicleFeature;
+//import org.springframework.boot.jackson.JsonComponent;
+//
+//import java.io.IOException;
+//import java.util.List;
+//
+//@JsonComponent
+//public class randomserialize extends JsonDeserializer<VehicleFeature> {
+//
+//
+//    @Override
+//    public VehicleFeature deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+//        ObjectCodec oc = jsonParser.getCodec();
+//        JsonNode node = oc.readTree(jsonParser);
+//        String exterior= node.get("Exterior").toString();
+//        //exterior = exterior.replaceAll("[^a-zA-Z]", "");
+//        String interior=node.get("Interior").toString();
+//        //interior = interior.replaceAll("[^a-zA-Z]", "");
+//        return new VehicleFeature(exterior,interior);
+//    }
+//}
